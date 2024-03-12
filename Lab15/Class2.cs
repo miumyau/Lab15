0318292b048ae9a7de6e08396eeb2afb14acf414
ArtiOnMoon <67146003+ArtiOnMoon@users.noreply.github.com>
2024-03-12 18:22:41 +0300

    123

---
+++ Lab15/Class2.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab15
+{
+    internal class GeomProgression : ISeries
+    {
+        int startvalue;
+        int step;
+        int currentvalue;
+        public int Startvalue { get { return startvalue; } set { startvalue = value; } }
+        public int Step { get { return step; } set { step = value; } }
+        public int Currentvalue { get { return currentvalue; } set { currentvalue = value; } }
+        public int GetNext()
+        {
+            Currentvalue *= Step;
+            return Currentvalue;
+        }
+
+        public void Reset()
+        {
+            Currentvalue = Startvalue;
+        }
+
+        public void SetStart(int x)
+        {
+            Startvalue = x;
+            Currentvalue = Startvalue;
+        }
+
+        public void SetStep(int s)
+        {
+            Step = s;
+        }
+    }
+}
+
--- Lab15/Program.cs
+++ Lab15/Program.cs
@@ -4,7 +4,33 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            ArithProgression arithProgression = new ArithProgression();
+            GeomProgression geomProgression = new GeomProgression();
+            arithProgression.SetStart(5);
+            arithProgression.SetStep(3);
+            Console.WriteLine(arithProgression.GetNext());
+            Console.WriteLine(arithProgression.GetNext());
+            Console.WriteLine(arithProgression.GetNext());
+            arithProgression.Reset();
+            Console.WriteLine(arithProgression.GetNext());
+            geomProgression.SetStart(1);
+            geomProgression.SetStep(2);
+            Console.WriteLine(geomProgression.GetNext());
+            Console.WriteLine(geomProgression.GetNext());
+            Console.WriteLine(geomProgression.GetNext());
+            Console.WriteLine(geomProgression.GetNext());
+            Console.WriteLine(geomProgression.GetNext());
+            Console.WriteLine(geomProgression.GetNext());
+            geomProgression.Reset();
+            Console.WriteLine(geomProgression.GetNext());
+            Console.WriteLine(geomProgression.GetNext());
+            Console.WriteLine(geomProgression.GetNext());
+            Console.ReadKey();
+
+
+
+
+
         }
     }
 }
